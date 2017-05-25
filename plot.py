41a826cf2127e67f2b8f62ed62956a36dfccdb49
Lucas John Jacobson <ljacobson64@gmail.com>
2017-05-25 14:06:53 -0500

    Make plots beautiful

--- plot.py
+++ plot.py
@@ -4,11 +4,16 @@ import numpy as np
 import matplotlib.colors as colors
 import matplotlib.cm as cm
 import matplotlib.pyplot as plt
+from matplotlib import rc
 from mpl_toolkits.mplot3d import Axes3D
 
 def main():
     to_file = True
     combined = False
+    dpi = 300
+
+    rc('font', **{'size': 10, 'family': 'serif', 'serif': ['Computer Modern Roman']})
+    rc('text', usetex=True)
 
     # Load data from file
     data_fwd = np.loadtxt('T_fwd.txt')
@@ -27,44 +32,39 @@ def main():
 
     if combined: fig = plt.figure(figsize=(24, 6))
 
-    if combined: ax1 = fig.add_subplot(1, 3, 1, projection='3d')
-    else: fig = plt.figure()
-    ax1 = fig.gca(projection='3d')
-    ax1.plot_surface(xp, yp, data_fwd, rstride = 1, cstride = 1,
-                     linewidth = 0, cmap = cm.get_cmap('Spectral_r'))
-    ax1.set_ylim([-0.5*(Lmax - Lx), Lx + 0.5*(Lmax - Lx)])
-    ax1.set_ylim([-0.5*(Lmax - Ly), Ly + 0.5*(Lmax - Ly)])
-    ax1.set_xlabel('x [m]')
-    ax1.set_ylabel('y [m]')
-    ax1.set_zlabel('Temperature [K]')
-    ax1.set_title('Forward solution')
-    if to_file: plt.savefig('forward.png', dpi=300); plt.close()
-
-    if combined: ax1 = fig.add_subplot(1, 3, 2, projection='3d')
-    else: fig = plt.figure()
-    ax2 = fig.gca(projection='3d')
-    ax2.plot_surface(xp, yp, data_adj, rstride = 1, cstride = 1,
-                     linewidth = 0, cmap = cm.get_cmap('Spectral_r'))
-    ax2.set_ylim([-0.5*(Lmax - Lx), Lx + 0.5*(Lmax - Lx)])
-    ax2.set_ylim([-0.5*(Lmax - Ly), Ly + 0.5*(Lmax - Ly)])
-    ax2.set_xlabel('x [m]')
-    ax2.set_ylabel('y [m]')
-    ax2.set_zlabel('Adjoint temperature [m^3-K/W]')
-    ax2.set_title('Adjoint solution')
-    if to_file: plt.savefig('adjoint.png', dpi=300); plt.close()
-
-    if combined: ax1 = fig.add_subplot(1, 3, 3, projection='3d')
-    else: fig = plt.figure()
-    ax3 = fig.gca(projection='3d')
-    ax3.plot_surface(xp, yp, data_fwd*data_adj, rstride = 1, cstride = 1,
-                     linewidth = 0, cmap = cm.get_cmap('Spectral_r'))
-    ax3.set_ylim([-0.5*(Lmax - Lx), Lx + 0.5*(Lmax - Lx)])
-    ax3.set_ylim([-0.5*(Lmax - Ly), Ly + 0.5*(Lmax - Ly)])
-    ax3.set_xlabel('x [m]')
-    ax3.set_ylabel('y [m]')
-    ax3.set_zlabel('Forward * adjoint')
-    ax3.set_title('Contributon')
-    if to_file: plt.savefig('contributon.png', dpi=300); plt.close()
+    for i in range(3):
+        if combined: ax = fig.add_subplot(1, 3, i, projection='3d')
+        else: fig = plt.figure()
+        ax = fig.gca(projection='3d')
+        if   i == 0: data = data_fwd
+        elif i == 1: data = data_adj
+        elif i == 2: data = data_fwd * data_adj
+        ax.plot_surface(xp, yp, data, rstride = 1, cstride = 1,
+                        linewidth = 0, cmap = cm.get_cmap('Spectral_r'))
+        ax.set_xlim([-0.5*(Lmax - Lx), Lx + 0.5*(Lmax - Lx)])
+        ax.set_ylim([-0.5*(Lmax - Ly), Ly + 0.5*(Lmax - Ly)])
+        xlims = ax.get_xlim()
+        ylims = ax.get_ylim()
+        zlims = ax.get_zlim()
+        ax.set_zlim([0, zlims[1]])
+        ax.ticklabel_format(axis='z', style='sci', scilimits=(0, 0))
+        ax.zaxis.get_offset_text().set_visible(False)
+        e_str = r'$10^{%u}$' % int(np.floor(np.log10(zlims[1])))
+        ax.text(1.05 * xlims[1], 1.05 * ylims[1], 1.05 * zlims[1], e_str)
+        ax.set_xlabel(r'x [m]')
+        ax.set_ylabel(r'y [m]')
+        if i == 0:
+            ax.set_zlabel(r'Temperature [K]')
+            ax.set_title(r'Forward solution')
+            if to_file: plt.savefig('forward.png', dpi=dpi); plt.close()
+        elif i == 1:
+            ax.set_zlabel(r'Adjoint temperature $\left[\frac{\textup{m}^3\textup{-K}}{\textup{W}}\right]$')
+            ax.set_title(r'Adjoint solution')
+            if to_file: plt.savefig('adjoint.png', dpi=dpi); plt.close()
+        elif i == 2:
+            ax.set_zlabel(r'Forward $\times$ adjoint')
+            ax.set_title(r'Contributon')
+            if to_file: plt.savefig('contributon.png', dpi=dpi); plt.close()
 
     if not to_file: plt.show()
 
